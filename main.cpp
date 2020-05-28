5ff3a69253d50a8d7b3391549ebec18b8fdfb2b6
michael <michael_kirsch@t-online.de>
2020-05-28 11:10:47 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 //T(n) = n*T(n-1)+n
 
-int recourse(int n , int* stop)
+int recourse(int n , int& counter, int stop)
 {
-    if(n==*stop)
+    counter++;
+
+    if(counter>=stop)
         return n;
-    return n*recourse(n-1,stop)+n;
+    return n*recourse(n-1,counter,stop)+n;
 }
 
+
+void rec(int stop)
+{
+
+}
+
+
+
 int main() {
-    int stopvalue = 10;
-    std::cout << recourse(1,&stopvalue) << std::endl;
+    int stopvalue=0;
+    std::cout << recourse(5,stopvalue,1000) << std::endl;
     return 0;
 }
